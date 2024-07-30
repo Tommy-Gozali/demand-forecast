14ea2636f63a48a53ae7f1d836c08fa3c5f09561
Tommy Gozali <Tommy.gozali97@gmail.com>
2024-07-30 06:35:05 +0200

    update backtesting with costum backtest class

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,58 +1,44 @@
 import os
 import joblib
-from data_processing import steps, data, data_train, data_test, data_val, exog_vars, hour_in_week
-from sklearn.metrics import mean_absolute_percentage_error
-from skforecast.model_selection import backtesting_forecaster
 import pandas as pd
+from data_processing import data, data_train, data_val, data_test, exog_vars, hour_in_week
+from sklearn.metrics import mean_absolute_percentage_error
+from forecast import BacktestingForecaster
 
 cwd = os.path.dirname(__file__)
-modelling_file_path = os.path.join(cwd, "results_gpu_train_XGBRegressor_24_steps.joblib")
+modelling_file_path = os.path.join(cwd, './modelling/model/forecaster.joblib')
 backtest_result_file_path = os.path.join(cwd, '../data/backtest_result.csv')
 
 forecaster = joblib.load(modelling_file_path)
 regressor_name = str(forecaster.regressor).split('(')[0]
+
 data_train_val = pd.concat([data_train, data_val])
 
-# Predictions
-# ==============================================================================
-def predict_single_forecast():
-    predictions = forecaster.predict(steps = steps,
-                                    exog  = data_test[exog_vars])
-
-    predictions.index = data_test.index
-    print(forecaster.__dict__)
-
-    data_test[regressor_name] = predictions
-    fig = data_test[["y", "Most recent forecast", regressor_name, "Week-ahead forecast"]].plot.line()
-    fig.show()
-
-    for col in ["Most recent forecast",regressor_name , "Week-ahead forecast"]:
-        error_mape = mean_absolute_percentage_error(
-                        y_true = data_test['y'],
-                        y_pred = data_test[col]
-                    )
-    return print(f"Test error (MAPE) for {col}: {error_mape*100}%")
+bf = BacktestingForecaster(
+    forecaster = forecaster,
+    y = data["y"],
+    steps = hour_in_week,
+    exog = data[exog_vars],
+    metric = mean_absolute_percentage_error,
+    initial_train_size = len(data_train),
+    refit = True,
+)
 
 def backtest():
-    metric, predictions = backtesting_forecaster(
-                          forecaster         = forecaster,
-                          y                  = data['y'],
-                          steps              = hour_in_week,
-                          exog               = data[exog_vars],
-                          metric             = 'mean_absolute_percentage_error',
-                          initial_train_size = len(data_train_val),
-                          refit              = True,
-                          n_jobs             = 'auto',
-                          verbose            = True,
-                          show_progress      = True
-                      )
-    #print(f"Test error (MAPE) for {col}: {metric*100}%")
+    metric, predictions = bf.backtesting_forecaster()
+    print(f"Test error (MAPE) for {regressor_name}: {metric*100}%")
     predictions.index = data_test.index
     data_test[regressor_name] = predictions
-
+    """
     for i, fold in enumerate(predictions):
         print(f"Fold {i}: {fold}")
-        
+    """    
     return data_test.to_csv(backtest_result_file_path, index = False)
 
-backtest()
+#backtest()
+folds_int = bf._create_backtesting_folds()
+folds = bf.get_backtesting_folds(
+    backtest_indexes= folds_int,
+    backtest_result_df= pd.read_csv(backtest_result_file_path),
+    regressor_name= regressor_name,)
+
